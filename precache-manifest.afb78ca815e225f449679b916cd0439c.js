32bbb6f351eb0d2a395cd8654aa0aea51fa91530
pSiemieniuk <psiemieniuk@thebrain.pro>
2020-07-21 12:56:28 +0200

    Updates

---
+++ precache-manifest.afb78ca815e225f449679b916cd0439c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbf516de6c86e83966d342bad685c93c",
+    "url": "/JsonSchema-DynamicForm/index.html"
+  },
+  {
+    "revision": "66057fd728c68334afa5",
+    "url": "/JsonSchema-DynamicForm/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6bf322b2da5b2d555060",
+    "url": "/JsonSchema-DynamicForm/static/js/2.d357eae5.chunk.js"
+  },
+  {
+    "revision": "77e7f328dacc981fca681ee4ae94d669",
+    "url": "/JsonSchema-DynamicForm/static/js/2.d357eae5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66057fd728c68334afa5",
+    "url": "/JsonSchema-DynamicForm/static/js/main.46c53c12.chunk.js"
+  },
+  {
+    "revision": "6a6a77ee484f60d957e5",
+    "url": "/JsonSchema-DynamicForm/static/js/runtime-main.7550f605.js"
+  }
+]);
